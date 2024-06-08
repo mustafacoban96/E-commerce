def7dc57d8be840e10c89c0d0f46a6bc6c9b5303
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-06-08 14:09:12 +0300

    spring sec depend - jwt service - jwt.key-expiration

---
+++ E-commerce/src/main/java/com/shepherd/E_commerce/service/securityService/JwtService.java
@@ -0,0 +1,87 @@
+package com.shepherd.E_commerce.service.securityService;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+
+
+@Service
+public class JwtService {
+	
+	@Value("${jwt.key}")
+	private String SECRET;
+	
+	
+	@Value("${jwt.expiration}")
+	private long jwtExpiration;
+	
+	
+	//Generate Token function
+	public String generteToken(String username) {
+		
+		Map<String, Object> claims = new HashMap<>();
+		
+		return createToken(claims,username,jwtExpiration);
+	}
+	
+	private String createToken(Map<String, Object> claims,String username,long jwtExpiration) {
+		
+		return Jwts.builder()
+				.setClaims(claims)
+				.setSubject(username)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+				.signWith(getSignKey(),SignatureAlgorithm.HS256)
+				.compact();
+	}
+	
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		String username = extractEmail(token);
+		Date expiration = extractExpiration(token);
+		return userDetails.getUsername().equals(username) && expiration.after(new Date());
+	}
+	
+	
+	private Date extractExpiration(String token) {
+		Claims claims = Jwts
+				.parserBuilder()
+				.setSigningKey(getSignKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		return claims.getExpiration();
+				
+	}
+	
+	private String extractEmail(String token) {
+		Claims claims = Jwts
+				.parserBuilder()
+				.setSigningKey(getSignKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		return claims.getSubject();
+	}
+	
+	
+	
+	
+}
