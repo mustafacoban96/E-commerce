4b6aa4d880a07046508836f3fb3694122c828796
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-07-23 00:55:08 +0300

    cart repo findById

--- E-commerce/src/main/java/com/shepherd/E_commerce/repository/CartRepository.java
+++ E-commerce/src/main/java/com/shepherd/E_commerce/repository/CartRepository.java
@@ -1,8 +1,10 @@
 package com.shepherd.E_commerce.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import com.shepherd.E_commerce.models.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shepherd.E_commerce.models.Cart;
@@ -11,4 +13,6 @@ import com.shepherd.E_commerce.models.User;
 public interface CartRepository extends JpaRepository<Cart, UUID>{
 
     Optional<Object> findByUserId(UUID user_id);
+    List<Products> findAllById(UUID cart_id);
+
 }
