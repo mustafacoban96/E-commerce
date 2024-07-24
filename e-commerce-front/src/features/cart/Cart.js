cb7b9c5ff507bae7ca5a00890103a34d4f5a71ef
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-07-25 02:08:57 +0300

    responsive cart list (not enough)

--- e-commerce-front/src/features/cart/Cart.js
+++ e-commerce-front/src/features/cart/Cart.js
@@ -17,13 +17,13 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const Cart = () => {
     const [quantity, setQuantity] = useState(1);
-    const [totalPrice,setTotalPrice] = useState()
+    
   return (
    <Box 
-    sx={{display:'flex',justifyContent:'space-around'}}
+    sx={{display:'flex',justifyContent:'space-around',flexDirection:{md:'row',xs:'column'}}}
    >
     {/* cart item area */}
-    <Box sx={{width:'65%'}}>
+    <Box sx={{width:{lg:'65%',md:'50%',xs:'90%'},marginX:'6%'}}>
         {
             listee.map((p,index) =>(
                 <Item key={p} elevation={6}>
@@ -31,12 +31,12 @@ const Cart = () => {
                 component="img"
                 image='https://lp2.hm.com/hmgoepprod?set=source[/ac/74/ac7495fa2aa2a0239a36e19102381cee9b5a0466.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]'
                 alt="green iguana"
-                sx={{maxWidth:120}}
+                sx={{maxWidth:{md:120,sm:100}}}
             />
           
             <Stack direction={'column'}>
                 <Typography variant='h6' sx={{fontWeight:'bold'}}>Product name</Typography>
-                <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in orci a leo varius feugiat. Phasellus in felis sed elit eleifend mollis non ut sem. Donec a venenatis purus. </Typography>
+                <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in orci a leo varius feugiat. Donec a venenatis purus. </Typography>
                 <Stack direction={'row'} sx={{marginTop:'5px',textAlign:'center'}} spacing={1}>
                     <Typography variant='h7' sx={{fontWeight:'bold'}}>Price :</Typography>
                     <Typography  variant='h7'>12 ₺</Typography>
@@ -60,7 +60,7 @@ const Cart = () => {
                             const value = Math.max(1, Math.min(Number(e.target.value), 10));
                             setQuantity(value);
                         }}
-                        sx={{width:'10%'}}
+                        sx={{width:{lg:'10%',md:'12%'}}}
                     />
                     <IconButton onClick={() => setQuantity(prev => Math.min(prev + 1, 10))} size="small">
                         <AddCircleOutlineIcon fontSize="inherit" />
@@ -88,29 +88,48 @@ const Cart = () => {
         
         
     </Box>
-    <Box sx={{width:'30%',p:2,position:'relative'}}>
-        <Box sx={{position:'fixed',boxShadow:'1px 1px 1px 1.4px lightgrey',padding:3}}>
-        <Stack direction={'column'} spacing={3}>
-            <Typography sx={{fontSize:'1.2em',fontWeight:'bold',color:'black'}}>Summary</Typography>
-            <Stack direction={'row'} spacing={25}>
-                <Typography sx={{fontSize:'1.1em'}}>Subtotal</Typography>
-                <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
-            </Stack>
+        <Box sx={{width:{xl:'30%',lg:'35%',md:'40%'},p:2}}>
+            <Box sx={{position:'fixed',boxShadow:'1px 1px 1px 1.4px lightgrey',padding:3}}>
+            <Stack direction={'column'} spacing={1.5}>
+                <Typography sx={{fontSize:'1.2em',fontWeight:'bold',color:'black'}}>Summary</Typography>
+                <Stack direction={'row'} spacing={25}>
+                    <Typography sx={{fontSize:'1.1em'}}>Subtotal</Typography>
+                    <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
+                </Stack>
 
-            <Stack direction={'row'} spacing={25}>
-                <Typography sx={{fontSize:'1.1em'}}>Shipping</Typography>
-                <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
+                <Stack direction={'row'} spacing={25}>
+                    <Typography sx={{fontSize:'1.1em'}}>Shipping</Typography>
+                    <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
+                </Stack>
+                <Divider />
+                <Stack direction={'row'} spacing={23}>
+                    <Typography sx={{fontSize:'1.1em',fontWeight:'bold',color:'black'}}>Total Price</Typography>
+                    <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
+                </Stack>  
+                <Button variant="contained" sx={{backgroundColor:'black'}}>Order</Button>
             </Stack>
-            <Divider />
-            <Stack direction={'row'} spacing={23}>
-                <Typography sx={{fontSize:'1.1em',fontWeight:'bold',color:'black'}}>Total Price</Typography>
-                <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
-            </Stack>  
-            <Button variant="contained" sx={{backgroundColor:'black'}}>Order</Button>
-        </Stack>
+            </Box>
         </Box>
-        
-    </Box>
+        <Box sx={{display:{xs:'flex',md:'none'},justifyContent:'center',padding:'25px'}}>
+        <Stack direction={'column'} spacing={1.5} sx={{width:{sm:'80%',xs:'90%'}}}>
+                <Typography sx={{fontSize:'1.2em',fontWeight:'bold',color:'black'}}>Summary</Typography>
+                <Stack direction={'row'} spacing={25}>
+                    <Typography sx={{fontSize:'1.1em'}}>Subtotal</Typography>
+                    <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
+                </Stack>
+
+                <Stack direction={'row'} spacing={25}>
+                    <Typography sx={{fontSize:'1.1em'}}>Shipping</Typography>
+                    <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
+                </Stack>
+                <Divider />
+                <Stack direction={'row'} spacing={23}>
+                    <Typography sx={{fontSize:'1.1em',fontWeight:'bold',color:'black'}}>Total Price</Typography>
+                    <Typography sx={{fontSize:'1em'}}>200 ₺</Typography>
+                </Stack>  
+                <Button variant="contained" sx={{backgroundColor:'black'}}>Order</Button>
+            </Stack>
+        </Box>
    </Box>
   )
 }
