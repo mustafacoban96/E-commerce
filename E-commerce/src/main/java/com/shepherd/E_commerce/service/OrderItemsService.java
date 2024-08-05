ea7380ae81ab9d4dd42b777d99c97f7f9c7a87ce
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-08-06 01:48:23 +0300

    define order system service method

--- E-commerce/src/main/java/com/shepherd/E_commerce/service/OrderItemsService.java
+++ E-commerce/src/main/java/com/shepherd/E_commerce/service/OrderItemsService.java
@@ -1,8 +1,15 @@
 package com.shepherd.E_commerce.service;
 
 
+import com.shepherd.E_commerce.dto.requests.OrderItemRequest;
+import com.shepherd.E_commerce.models.OrderItems;
+
+import java.util.UUID;
 
 public interface OrderItemsService {
-	
-	
+
+    public OrderItems createOrderItem(OrderItemRequest request, UUID order_id);
+
+
+
 }
--- E-commerce/src/main/java/com/shepherd/E_commerce/service/OrderService.java
+++ E-commerce/src/main/java/com/shepherd/E_commerce/service/OrderService.java
@@ -1,5 +1,14 @@
 package com.shepherd.E_commerce.service;
 
+import com.shepherd.E_commerce.dto.requests.OrderRequest;
+import com.shepherd.E_commerce.models.Orders;
+
+import java.util.UUID;
+
+
 public interface OrderService {
+
+    public Orders createOrder(OrderRequest request);
+    public Orders getOrderById(UUID order_id);
 
 }
