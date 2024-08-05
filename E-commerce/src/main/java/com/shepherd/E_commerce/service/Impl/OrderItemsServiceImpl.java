6675389b4fdd651f02a937c76aec23b1798fa1b7
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-08-06 01:49:00 +0300

    Order and order items impl service

--- E-commerce/src/main/java/com/shepherd/E_commerce/service/Impl/OrderItemsServiceImpl.java
+++ E-commerce/src/main/java/com/shepherd/E_commerce/service/Impl/OrderItemsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.shepherd.E_commerce.service.Impl;
 
+import com.shepherd.E_commerce.dto.requests.OrderItemRequest;
+import com.shepherd.E_commerce.service.OrderService;
+import com.shepherd.E_commerce.service.ProductService;
 import org.springframework.stereotype.Service;
 
 
@@ -7,10 +10,35 @@ import com.shepherd.E_commerce.models.OrderItems;
 import com.shepherd.E_commerce.repository.OrderItemsRepository;
 import com.shepherd.E_commerce.service.OrderItemsService;
 
+import java.util.UUID;
+
 @Service
 public class OrderItemsServiceImpl implements OrderItemsService{
-	
-	
-	
 
+    private final OrderItemsRepository orderItemsRepository;
+    private final ProductService productService;
+
+    private final OrderService orderService;
+    public OrderItemsServiceImpl(OrderItemsRepository orderItemsRepository,
+                                 ProductService productService,
+                                 OrderService orderService) {
+        this.orderItemsRepository = orderItemsRepository;
+        this.productService = productService;
+        this.orderService = orderService;
+    }
+
+    @Override
+    public OrderItems createOrderItem(OrderItemRequest request, UUID order_id) {
+
+        OrderItems item = OrderItems.builder()
+                .name(request.getProduct_name())
+                .quantity(request.getQuantity())
+                .unit_price(request.getUnit_price())
+                .total_price(request.getTotal_price_per_product())
+                .products(productService.findProductById(request.getProduct_id()))
+                .build();
+
+        item.setOrder(orderService.getOrderById(order_id));
+        return orderItemsRepository.save(item);
+    }
 }
--- E-commerce/src/main/java/com/shepherd/E_commerce/service/Impl/OrderServiceImpl.java
+++ E-commerce/src/main/java/com/shepherd/E_commerce/service/Impl/OrderServiceImpl.java
@@ -1,10 +1,63 @@
 package com.shepherd.E_commerce.service.Impl;
 
+import com.shepherd.E_commerce.dto.requests.OrderItemRequest;
+import com.shepherd.E_commerce.dto.requests.OrderRequest;
+import com.shepherd.E_commerce.models.OrderItems;
+import com.shepherd.E_commerce.models.Orders;
+import com.shepherd.E_commerce.repository.OrdersRepository;
+import com.shepherd.E_commerce.service.OrderItemsService;
+import com.shepherd.E_commerce.service.UserService;
+import org.hibernate.query.Order;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import com.shepherd.E_commerce.service.OrderService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class OrderServiceImpl implements OrderService{
 
+    private final UserService userService;
+    private final OrderItemsService orderItemsService;
+
+    private final OrdersRepository ordersRepository;
+
+    public OrderServiceImpl(UserService userService,
+                            @Lazy OrderItemsService orderItemsService,
+                            OrdersRepository ordersRepository) {
+        this.userService = userService;
+        this.orderItemsService = orderItemsService;
+        this.ordersRepository = ordersRepository;
+    }
+
+    //create order
+    @Override
+    @Transactional
+    public Orders createOrder(OrderRequest request) {
+
+        Set<OrderItems> items = new HashSet<>();
+        Orders orders = Orders.builder()
+                .total_price(request.getTotal_price())
+                .user(userService.getUserByIdAsEntity(request.getUser_id()))
+                .orderItems(items)
+                .build();
+        ordersRepository.save(orders);
+        //System.out.println("Hedef ustanın elinde:::::" + orders.getId());
+        for(OrderItemRequest item : request.getOrder_items_list()){
+            items.add(orderItemsService.createOrderItem(item,orders.getId()));
+        }
+        orders.setOrderItems(items);
+        return ordersRepository.save(orders);
+    }
+
+    @Override
+    public Orders getOrderById(UUID order_id) {
+        return ordersRepository.getReferenceById(order_id);
+    }
+    //////////////////////////////////////////////////////////create order
 }
