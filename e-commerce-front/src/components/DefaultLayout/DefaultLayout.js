5ba65bce3329cf3feb8a75a90780febe456d6270
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-08-12 00:26:05 +0300

    revise fetch cart items for cart length to init

--- e-commerce-front/src/components/DefaultLayout/DefaultLayout.js
+++ e-commerce-front/src/components/DefaultLayout/DefaultLayout.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuthContext } from '../../context/AuthContext'
 import { Navigate, Outlet } from 'react-router';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-
+import { useDispatch } from 'react-redux';
+import { fetchCartItems } from '../../features/cart/cartSlice';
+  
 const DefaultLayout = () => {
 
-  const {access_token} = useAuthContext();
+  const {access_token,user} = useAuthContext();
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchCartItems());  // Make sure this action fetches items from the server
+    }
+  }, [user, dispatch]);
 
   if(!access_token){
     return <Navigate to='login'/>
