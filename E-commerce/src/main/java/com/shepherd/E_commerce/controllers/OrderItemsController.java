7de732123065fb7965d8f3bc7dcc22f82057aac0
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-08-17 01:08:34 +0300

    get order items by using order id API

--- E-commerce/src/main/java/com/shepherd/E_commerce/controllers/OrderItemsController.java
+++ E-commerce/src/main/java/com/shepherd/E_commerce/controllers/OrderItemsController.java
@@ -1,17 +1,19 @@
 package com.shepherd.E_commerce.controllers;
 
+import com.shepherd.E_commerce.dto.response.OrderItemsByOrderIdResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.shepherd.E_commerce.service.OrderItemsService;
 
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/orderItems")
+@RequestMapping("/api/v1/order-items")
 public class OrderItemsController {
 	
 	private final OrderItemsService orderItemsService;
@@ -19,6 +21,17 @@ public class OrderItemsController {
 	public OrderItemsController(OrderItemsService orderItemsService) {
 		this.orderItemsService = orderItemsService;
 	}
+
+
+	@GetMapping("/{order_id}")
+	public ResponseEntity<List<OrderItemsByOrderIdResponse>> getOrderItemsByOrderId(@PathVariable("order_id") UUID order_id){
+		List<OrderItemsByOrderIdResponse> response = orderItemsService.getOrderItemsByOrderId(order_id);
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+
+
+
 	
 	
 	
