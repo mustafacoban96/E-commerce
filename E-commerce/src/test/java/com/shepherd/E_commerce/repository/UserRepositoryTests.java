0422ea36d2cbf8dc154b601de7f5d0f691cec2ed
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-08-27 18:54:33 +0300

    revise unit test for user repository

--- E-commerce/src/test/java/com/shepherd/E_commerce/repository/UserRepositoryTests.java
+++ E-commerce/src/test/java/com/shepherd/E_commerce/repository/UserRepositoryTests.java
@@ -2,6 +2,7 @@ package com.shepherd.E_commerce.repository;
 
 
 
+import com.shepherd.E_commerce.models.Products;
 import com.shepherd.E_commerce.models.Roles;
 import com.shepherd.E_commerce.models.User;
 import org.assertj.core.api.Assertions;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -26,7 +28,6 @@ public class UserRepositoryTests {
     void givenNewUser_whenSave_thenSuccess(){
 
         //given
-
         User user = User.builder()
                 .username("exampleUser")
                 .password("password")
@@ -94,6 +95,39 @@ public class UserRepositoryTests {
         userRepository.save(user);
         User existUser = userRepository.getReferenceByEmail("user@example.com");
         Assertions.assertThat(existUser).isNotNull();
+
+    }
+
+    @Test
+    void testFindAllProduct(){
+
+        List<User> userList = userRepository.findAll();
+
+        Assertions.assertThat(userList).hasSizeGreaterThan(0);
+    }
+
+    @Test
+    void testDeleteById(){
+
+        User user = User.builder()
+                .username("exampleUser")
+                .password("password")
+                .email("user@example.com")
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .isEnabled(true)
+                .authorities(Set.of(Roles.ROLE_USER))  // Assign the ROLE_USER to this user
+                .build();
+        userRepository.save(user);
+
+        // Delete the product by its ID
+        userRepository.deleteById(user.getId());
+
+        // Verify that deleteById was called once with the correct ID
+        Optional<User> deletedUser = userRepository.findById(user.getId());
+
+        Assertions.assertThat(deletedUser).isEmpty();
 
     }
 }
