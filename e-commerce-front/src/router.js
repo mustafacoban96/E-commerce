0ce9da28a2a37d475a75d0783f92c0548aae4d6a
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-10-06 12:11:08 +0300

    profile information router

--- e-commerce-front/src/router.js
+++ e-commerce-front/src/router.js
@@ -14,6 +14,7 @@ import Order from "./features/order/Order";
 import EndOfOrder from "./features/order/EndOfOrder";
 import OrderList from "./features/order/OrderList";
 import OrderDetail from "./features/order/OrderDetail";
+import ProfileSettingsPage from "./features/profileSettings/ProfileSettingsPage";
 
 
 
@@ -65,6 +66,10 @@ const router = createBrowserRouter([
                 element:<OrderDetail/>,
                 path:'/order/:orderId',
                
+            },
+            {
+                element:<ProfileSettingsPage/>,
+                path:"/profile-information"
             }
         ]
     },
